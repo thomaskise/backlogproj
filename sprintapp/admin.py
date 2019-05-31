a68527c2eec006675ba016f9f902942ed41e2ac3
Thom Harrington <thomas.harrington@seattlecentral.edu>
2019-05-31 13:20:09 -0700

    adds backlog page

--- sprintapp/admin.py
+++ sprintapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Status_lkup, Priority_lkup, Points_lkup, Roles_lkup, Story, Task, Teammember, Sprint
 
 # Register your models here.
+admin.site.register(Status_lkup)
+admin.site.register(Priority_lkup)
+admin.site.register(Points_lkup)
+admin.site.register(Roles_lkup)
+admin.site.register(Sprint)
+admin.site.register(Story)
+admin.site.register(Task)
+admin.site.register(Teammember)
--- sprintapp/views.py
+++ sprintapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Status_lkup, Priority_lkup, Points_lkup, Roles_lkup, Story, Task, Teammember, Sprint
 
 # Create your views here.
 def index (request):
     return render(request, 'sprintapp/index.html')
+
+def getbacklog(request):
+    story_list=Story.objects.all()
+    return render(request, 'sprintapp/backlog.html',{'backlog': story_list})
